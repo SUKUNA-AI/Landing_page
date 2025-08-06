9cc90ea45ab7878d610073fb9cb8e8a0ae2ecf90
Sukuna-AI <ilyakoryaev@yandex.ru>
2025-08-06 14:25:06 +0300

    Обновлен для аутентификации

--- app/config.py
+++ app/config.py
@@ -10,7 +10,9 @@ class Settings(BaseSettings):
     DATABASE_URL: str | None = None
     JWT_SECRET_KEY: str = "your-secret-key-change-this"  # Секретный ключ для JWT
     QWEN_API_KEY: str = "your-qwen-api-key"  # API-ключ для Qwen
-
+    ADMIN_USERNAME: str
+    ADMIN_PASSWORD: str
+    ADMIN_EMAIL: str
     @model_validator(mode='after')
     def get_database_url(self):
         self.DATABASE_URL = f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
